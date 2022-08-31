3a033eb3927520a68cb9f3b7a85840a26f0b7872
Bryan Lee <bryanmylee@gmail.com>
2022-08-31 11:16:57 +1000

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+  import Dialog from "@ally-ui/svelte-dialog";
+  import svelteLogo from "./assets/svelte.svg";
+  import Counter from "./lib/Counter.svelte";
+</script>
+
+<main>
+  <Dialog.Root>
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite Logo" />
+      </a>
+      <a href="https://svelte.dev" target="_blank">
+        <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+      </a>
+    </div>
+    <Dialog.Trigger>Open dialog</Dialog.Trigger>
+    <Dialog.Content>
+      <Dialog.Title>Vite + Svelte</Dialog.Title>
+
+      <div class="card">
+        <Counter />
+      </div>
+      <Dialog.Close>CLose dialog</Dialog.Close>
+    </Dialog.Content>
+  </Dialog.Root>
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank"
+      >SvelteKit</a
+    >, the official Svelte app framework powered by Vite!
+  </p>
+
+  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
